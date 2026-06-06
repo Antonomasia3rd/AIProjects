6d74cbe93bcc78258a63894c941447b0012cf227
Antonomasia <89201774+Antonomasia3rd@users.noreply.github.com>
2026-06-06 13:52:07 +0700

    Make desktop baseline header self-contained

--- dependencies/desktop_app_baseline.h
+++ dependencies/desktop_app_baseline.h
@@ -2,6 +2,17 @@
 #define AIP_DEPENDENCIES_DESKTOP_APP_BASELINE_H
 
 #include "baseline_app.h"
+
+#include <shellapi.h>
+
+#include <algorithm>
+#include <climits>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <cwchar>
+#include <mutex>
+
 #include "core.inc"
 #include "config_ini.inc"
 #include "command_line.inc"
